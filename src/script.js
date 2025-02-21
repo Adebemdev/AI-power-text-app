c219c7a5975a331f9bcf15f8440314d3a7eb5ac8
Adebem <adeolabusayo1995@gmail.com>
2025-02-21 04:02:18 +0100

    files updated

--- src/script.js
+++ src/script.js
@@ -1,9 +1,55 @@
 (async () => {
-  if (!('translation' in self) || !('createDetector' in self.translation)) {
+  if ('ai' in self && 'languageDetector' in self.ai) {
+    // The Language Detector API is available.
     document.querySelector('.header-message').hidden = false;
+    console.log('Yes');
     return;
   }
+})();
 
-  const detector = await self.translation.createDetector();
-  console.log(detector);
+(async function detectLanguage() {
+  // ELEMENT SELECTED
+  const messageInput = document.getElementById('input');
+  const messageOut = document.getElementById('output');
+  const sendButton = document.getElementById('button');
+  const detected = document.querySelector('.detect-text');
+
+  // LANGUAGE DETECTOR API
+  const languageDetectorCapabilities =
+    await self.ai.languageDetector.capabilities();
+  const detector = await self.ai.languageDetector.create();
+  console.log(detector, languageDetectorCapabilities);
+
+  // Function to send message
+  const sendMessage = async () => {
+    const message = messageInput.value.trim();
+    if (!message) {
+      detected.textContent = 'Not sure of which language you typed';
+      return;
+    }
+
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    messageElement.classList.add('message');
+
+    messageOut.appendChild(messageElement);
+    messageInput.value = '';
+
+    const { detectedLanguage, confidence } = (
+      await detector.detect(messageInput)
+    )[0];
+
+    detected.textContent = `I'm ${(confidence * 100).toFixed(
+      1
+    )} % sure that is the language you typed is`;
+  };
+
+  sendButton.addEventListener('click', sendMessage);
+  messageInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      sendMessage();
+    }
+  });
+
+  // DETECT LANGUAGE
 })();
